cc9e1b0206d7e9cb63613beb15a30c278946b0ae
SiddharthaChakrabarty <csiddhartha2004@gmail.com>
2025-12-17 11:57:34 +0000

    Model Page backend written

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from routes.upload import bp as upload
 from routes.chat_blueprint import bp as chat
 from routes.file_analysis_endpoint import analysis_bp
+from routes.models_blueprint import models_bp
 
 app = Flask(__name__)
 CORS(app)
@@ -11,6 +12,7 @@ CORS(app)
 app.register_blueprint(upload)
 app.register_blueprint(chat)
 app.register_blueprint(analysis_bp)
+app.register_blueprint(models_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
